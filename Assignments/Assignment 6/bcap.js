b6d08dacaf214b5746745840f20fcc122b19e7ab
bibsub422 <94613290+bibsub422@users.noreply.github.com>
2023-01-10 20:26:14 +0545

    Add bubbling and capture

---
+++ Assignments/Assignment 6/bcap.js
@@ -0,0 +1,10 @@
+var div=document.querySelector("div");
+var button=document.querySelector("button");
+
+div.addEventListener("click",()=>{
+console. log("div")
+},true)
+
+button.addEventListener("click",()=>{
+console. log("button")
+},true)
